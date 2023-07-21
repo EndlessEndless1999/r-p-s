fd68a2e2acb9b52dcbce88ea7b0054847a80318b
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-07-21 15:18:04 +0100

    finish game

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 import inquirer from 'inquirer';
 
-let choices = ['Rock', 'Paper', 'Scissors'];
+function restart(){
+    inquirer.prompt([{
+        type: 'list',
+        name: 'retry',
+        message: 'Play again?',
+        choices: ['Yes', 'No']
+    }]).then((answers) => {
+        if (answers.retry === 'Yes'){
+            startGame();
+        }
+    })
+}
+
+function startGame(){
+    let choices = ['Rock', 'Paper', 'Scissors'];
+let computerChoice = choices[Math.floor(Math.random()* choices.length)];
+let winningAnswer;
+let losingAnswer; 
+
+switch(computerChoice){
+    case 'Rock':
+        winningAnswer = 'Paper'
+        losingAnswer = 'Scissors'
+        break;
+    case 'Paper':
+        winningAnswer = 'Scissors'
+        losingAnswer = 'Rock'
+    case 'Scissors':
+        winningAnswer = 'Rock'
+        losingAnswer = 'Paper'
+}
 
 inquirer.prompt([
     {
+        type: 'list',
         name: 'start',
         message: 'Choose Rock, Paper or Scissors!',
         choices: ['Rock', 'Paper', 'Scissors']
     }
-]).then((answers)) => {
-
+]).then((answers) => {
+    if (answers.start === winningAnswer){
+        console.log('YOU WIN!')
+        restart();
+    }else if (answers.start === losingAnswer){
+        console.log('YOU LOSE')
+        restart();
+    }
+        else {
+        console.log('DRAW. REPLAY!');
+        startGame();
+    }
+}) 
 }
+
+
+startGame();
